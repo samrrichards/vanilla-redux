1a2fdf67aec6f0cb9fbb027a0a97fcd29c95ae77
Sam Richards <sam@zenithtutoring.com>
2018-01-16 19:17:10 -0800

    implemented redux in counter example

--- counter/main.js
+++ counter/main.js
@@ -1,15 +1,26 @@
-let count = 0;
+const DEFAULT_STATE = 0;
 
-const countValue = document.getElementById('count');
-function render() {
-  countValue.innerHTML = count.toString();
+function reducer(state=DEFAULT_STATE, action) {
+  if (action.type === "INCREMENT") return state + 1;
+
+  if (action.type === "DECREMENT") return state - 1;
+
+  return state;
 }
 
+const store = Redux.createStore(reducer);
+const countValue = document.getElementById('count');
+
+function render() {
+  countValue.innerHTML = store.getState().toString();
+}
+
+store.subscribe(render);
+
 document.getElementById('increment').addEventListener('click', () => {
- count++;
- render();
+ store.dispatch({type: "INCREMENT"});
 });
+
 document.getElementById('decrement').addEventListener('click', () => {
-  count--;
-  render();
+  store.dispatch({type: "DECREMENT"});
 });
